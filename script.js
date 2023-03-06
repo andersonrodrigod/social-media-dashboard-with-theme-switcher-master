8f42f079d72b9c8befa24a5b1fe0b76a5c18f1b2
Anderson Rodrigo <andersonhav@gmail.com>
2023-03-06 13:26:03 -0300

    consertando o sunnyside

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const btn = document.querySelector(".btn")
+const boxUps = document.querySelectorAll(".box-folowers-result")
+const boxDowns = document.querySelectorAll(".box-overview")
+const body = document.body
+const title = document.querySelectorAll(".title-1")
+
+
+
+
+
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("btn-mover") 
-    document.body.style.background= "white"
+    body.classList.toggle("body-background")
+    
+    
+    boxDowns.forEach(box => {
+        box.classList.toggle("boxes-colors") 
+    })
+
+    boxUps.forEach(box => {
+        box.classList.toggle("boxes-colors") 
+    })
+
+    title.forEach(tit => {
+        tit.classList.toggle("title-color")
+    })
     
 })
+
+
 
